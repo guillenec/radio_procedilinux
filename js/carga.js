df5808896701e0f3961bda4f891c33224214e291
guille <guillermoneculqueo@gmail.com>
2022-09-23 01:38:03 -0300

    arreglando medias para que se vea bien en telefono

--- js/carga.js
+++ js/carga.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', () =>{
 
-    setTimeout(carga, 1000); //lo dejo un seg despues que cargue la page
-    function carga(){
+    /*setTimeout(carga, 0); //lo dejo un seg despues que cargue la page
+    function carga(){ */
         document.getElementById('preloader').className='none';
         document.getElementById("body").classList.remove("hidden");
-    }
+        
 })
