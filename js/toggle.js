181413ad34e4f1a7b169f8c994702c80589845da
guille <guillermoneculqueo@gmail.com>
2022-09-28 02:29:21 -0300

    arreglando

--- js/toggle.js
+++ js/toggle.js
@@ -6,7 +6,6 @@ toggle_menu.onclick = function(){
     nav_menu.classList.toggle('active');
 
 }
-
 window.addEventListener('click', e=>{
     if (nav_menu.classList.contains('active') && e.target != nav_menu && e.target != toggle_menu )  {
         nav_menu.classList.toggle('active');
