b4ddfce30340de92af94458f5cc5e5ab3dd5f454
guille <guillermoneculqueo@gmail.com>
2022-09-28 01:22:41 -0300

    si clikeamos fuera del menu, ahora se repliega

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,14 @@
 let toggle_menu = document.querySelector('.toggle');
-let nav_menu = document.querySelector('.nav_menu')
+let nav_menu = document.querySelector('.nav_menu');
+let ul_menu =document.querySelector('menu');
+
 toggle_menu.onclick = function(){
     nav_menu.classList.toggle('active');
+
 }
+
+window.addEventListener('click', e=>{
+    if (nav_menu.classList.contains('active') && e.target != nav_menu && e.target != toggle_menu )  {
+        nav_menu.classList.toggle('active');
+    }
+} )
